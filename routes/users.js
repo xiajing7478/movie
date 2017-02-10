10aabb4283938ed1d67d6ed525f3df4f3cd5a1b6
xiajing7478 <260594736@qq.com>
2017-02-10 16:50:11 +0800

    add login and register

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var dal = require('../database/login.js');
 //var bcrypt = require('bcrypt');
 
 /* GET users listing. */
@@ -7,7 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('login',{title:'登录页'});
   //res.send('respond with a resource');
 }).post('/', function (req, res) {
+  dal.login(req.body, function (result) {
 
+  })
 });
 
 //bcrypt.genSalt
