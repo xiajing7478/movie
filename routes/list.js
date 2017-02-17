e6dac96157bc5d1add59b776e87233dc3b287e6f
xiajing7478 <260594736@qq.com>
2017-02-17 17:30:06 +0800

    add 完善数据的导出

--- routes/list.js
+++ routes/list.js
@@ -8,6 +8,7 @@ var dal = require('../database/admins');
 var mid = require('../database/middle');
 var category = require('../database/category');
 var fs = require('fs');
+var moment = require('moment');
 var nodeExcel = require('excel-export');
 var hbs = require('hbs');
 
@@ -69,55 +70,96 @@ router.post('/delete', function (req, res) {
 
 
 router.get('/exportExcel', function (req, res) {
+
+    dal.findAllMovies(function (movies) {
+        var conf ={},_arr=[],arr=[];
+        conf.stylesXmlFile = "styles.xml";
+
+        conf.cols=[
+            {caption:'编号',type:'number'},
+            {caption:'电影名称',type:'string'},
+            {caption:'国家',type:'string'},
+            {caption:'语言',type:'string'},
+            {caption:'年份',type:'number'},
+            {caption:'电影地址',type:'string'},
+            {caption:'电影海报',type:'string'},
+            {caption:'创建时间',type:'string'},
+            {caption:'更新时间',type:'string'},
+            {caption:'价格',type:'number'},
+            {caption:'导演',type:'string'},
+            {caption:'简述',type:'string'},
+            {caption:'电影分类',type:'number'},
+            {caption:'访问量',type:'number'},
+        ];
+        for(var item in movies){
+            _arr=[movies[item].id,movies[item].title,movies[item].country,movies[item].language,movies[item].year,movies[item].flash,movies[item].poster,movies[item].createTime,movies[item].updateTime,movies[item].price,movies[item].doctor,movies[item].summary,movies[item].categoryId,movies[item].pv];
+            arr.push(_arr);
+        }
+        conf.rows=arr;
+        var random = Math.random().toString().split('.')[1];
+        var result = nodeExcel.execute(conf);
+        res.setHeader('Content-Type', 'application/vnd.openxmlformats;charset=utf-8');
+        res.setHeader("Content-Disposition", "attachment; filename="+random+".xlsx");
+        res.end(result, 'binary');
+
+    })
+
     console.log('exportExcel ready.....');
-    var conf ={};
-    conf.stylesXmlFile = "styles.xml";
-    conf.cols = [{
-        caption:'string',
-        type:'string',
-        beforeCellWrite:function(row, cellData){
-            return cellData.toUpperCase();
-        },
-        width:28.7109375
-    },{
-        caption:'date',
-        type:'date',
-        beforeCellWrite:function(){
-            var originDate = new Date(Date.UTC(1899,11,30));
-            return function(row, cellData, eOpt){
-                if (eOpt.rowNum%2){
-                    eOpt.styleIndex = 1;
-                }
-                else{
-                    eOpt.styleIndex = 2;
-                }
-                if (cellData === null){
-                    eOpt.cellType = 'string';
-                    return 'N/A';
-                } else
-                    return (cellData - originDate) / (24 * 60 * 60 * 1000);
-            }
-        }()
-    },{
-        caption:'bool',
-        type:'bool'
-    },{
-        caption:'number',
-        type:'number'
-    }];
-    conf.rows = [
-        ['谢谢', new Date(Date.UTC(2013, 4, 1)), true, 3.14],
-        ["e", new Date(2012, 4, 1), false, 2.7182],
-        ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
-        ["null date", null, true, 1.414]
-    ];
 
 
-    var random = Math.random().toString().split('.')[1];
-    var result = nodeExcel.execute(conf);
-    res.setHeader('Content-Type', 'application/vnd.openxmlformats;charset=utf-8');
-    res.setHeader("Content-Disposition", "attachment; filename="+random+".xlsx");
-    res.end(result, 'binary');
+
+
+
+
+
+    //var conf ={};
+    //conf.stylesXmlFile = "styles.xml";
+    //conf.cols = [{
+    //    caption:'string',
+    //    type:'string',
+    //    beforeCellWrite:function(row, cellData){
+    //        return cellData.toUpperCase();
+    //    },
+    //    width:28.7109375
+    //},{
+    //    caption:'date',
+    //    type:'date',
+    //    beforeCellWrite:function(){
+    //        var originDate = new Date(Date.UTC(1899,11,30));
+    //        return function(row, cellData, eOpt){
+    //            if (eOpt.rowNum%2){
+    //                eOpt.styleIndex = 1;
+    //            }
+    //            else{
+    //                eOpt.styleIndex = 2;
+    //            }
+    //            if (cellData === null){
+    //                eOpt.cellType = 'string';
+    //                return 'N/A';
+    //            } else
+    //                return (cellData - originDate) / (24 * 60 * 60 * 1000);
+    //        }
+    //    }()
+    //},{
+    //    caption:'bool',
+    //    type:'bool'
+    //},{
+    //    caption:'number',
+    //    type:'number'
+    //}];
+    //conf.rows = [
+    //    ['谢谢', new Date(Date.UTC(2013, 4, 1)), true, 3.14],
+    //    ["e", new Date(2012, 4, 1), false, 2.7182],
+    //    ["M&M<>'", new Date(Date.UTC(2013, 6, 9)), false, 1.61803],
+    //    ["null date", null, true, 1.414]
+    //];
+    //
+    //
+    //var random = Math.random().toString().split('.')[1];
+    //var result = nodeExcel.execute(conf);
+    //res.setHeader('Content-Type', 'application/vnd.openxmlformats;charset=utf-8');
+    //res.setHeader("Content-Disposition", "attachment; filename="+random+".xlsx");
+    //res.end(result, 'binary');
 });
 
 function isLogin(req,res){
